7953398eb78120f5ce4fcfddd023a294942955cb
neo1989 <neosu89@gmail.com>
2013-07-25 17:42:21 +0800

    finish lists

--- lib/lists.py
+++ lib/lists.py
@@ -58,15 +58,85 @@ class UnorderedList:
             else:
                 previous = current
                 current = current.getNext()
-
         if found:
             previous.setNext(current.getNext())
 
     def toList(self):
-        l = []
+        res = []
         current = self.head
         while current != None:
-            l.append(current.getData())
+            res.append(current.getData())
             current = current.getNext()
-        return l
+        return res 
 
+class OrderedList:
+    def __init__(self):
+        self.head = None
+
+    def isEmpty(self):
+        return self.head == None
+
+    def add(self,item):
+        current = self.head
+        previous = None
+        stop = False
+        while current != None and not stop:
+            if current.getData() > item:
+                stop = True 
+            else:
+                previous = current
+                current = current.getNext()
+
+        temp = Node(item)
+        if previous == None:
+            temp.setNext(self.head)
+            self.head = temp
+        else:
+            temp.setNext(current)
+            previous.setNext(temp)
+
+    def length(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count += 1
+            current = current.getNext()
+        return count
+
+    def search(self,item):
+        current = self.head
+        found = False
+        stop = False 
+        while current != None and not found and not stop:
+            if current.getData() == item:
+                found = True
+            elif current.getData() > item:
+                stop = True
+            else:
+                current = current.getNext()
+        return found
+    
+    def remove(self,item):
+        current = self.head
+        previous = None
+        found = False
+        stop = False
+        while current != None and not found and not stop:
+            if current.getData() == item:
+                found = True
+            elif current.getData() > item:
+                stop = True
+            else:
+                previous = current
+                current = current.getNext()
+        if found:
+            previous.setNext(current.getNext())
+
+
+    def toList(self):
+        res = []
+        current = self.head
+        while current != None:
+            res.append(current.getData())
+            current = current.getNext()
+        return res 
--- tests/test_list.py
+++ tests/test_list.py
@@ -4,6 +4,7 @@ sys.path.append("lib");
 
 import unittest
 from lists import UnorderedList
+from lists import OrderedList
 
 class UnorderedListTestCase(unittest.TestCase):  
     def setUp(self):
@@ -28,12 +29,39 @@ class UnorderedListTestCase(unittest.TestCase):
         self.assertEqual(self.mylist.search(12),False)
         self.assertEqual(self.mylist.search(11),False)
         self.assertEqual(self.mylist.length(),5)
+
     def test_toList(self):
         self.assertEqual(self.mylist.toList(),[16,71,89,9,22,12])
 
+
+class OrderedListTestCase(unittest.TestCase):  
+    def setUp(self):
+        self.mylist = OrderedList()
+        self.mylist.add(12)
+        self.mylist.add(22)
+        self.mylist.add(9)
+        self.mylist.add(89)
+        self.mylist.add(71)
+        self.mylist.add(16)
+
+    def test_length(self):
+        self.assertEqual(self.mylist.length(),6)
+
+    def test_search(self):
+        self.assertEqual(self.mylist.search(89),True)
+        self.assertEqual(self.mylist.search(8),False)
+
+    def test_remove(self):
+        self.mylist.remove(12)
+        self.mylist.remove(11)
+        self.assertEqual(self.mylist.search(12),False)
+        self.assertEqual(self.mylist.search(11),False)
+        self.assertEqual(self.mylist.length(),5)
+
+    def test_toList(self):
+        self.assertEqual(self.mylist.toList(),[9,12,16,22,71,89])
+
+
+
 if __name__ == '__main__':
     unittest.main()
-
-
-
-
