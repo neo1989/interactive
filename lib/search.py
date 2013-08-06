4e4b840ba43f6cf64f43e053aba3e0adfe016898
neo1989 <neosu89@gmail.com>
2013-08-06 17:18:39 +0800

    add search

--- lib/search.py
+++ lib/search.py
@@ -1,14 +1,28 @@
+#coding:utf-8
 
 def sequentialSearch(alist,item):
     pos = 0
     found = False
-
     
-    while pos<len(alist) and not found:
+    while pos < len(alist) and not found:
         if alist[pos] == item:
             found = True
         else:
             pos += 1
 
+    return found
+
+def orderedSequentialSearch(alist,item):
+    pos = 0
+    found = False
+    stop = False
+
+    while pos < len(alist) and not found and not stop:
+        if alist[pos] == item:
+            found = True
+        elif alist[pos] > item:
+                stop = True
+        else:
+            pos +=  1 
 
     return found
--- tests/test_search.py
+++ tests/test_search.py
@@ -3,17 +3,42 @@ sys.path.append("lib")
 
 import unittest
 from search import * 
+from lists import * 
 
-class SearchTestCase(unittest.TestCase):  
+class sequentialSearchTestCase(unittest.TestCase):  
 
     def setUp(self):
-        self.testlist = [1,2,32,8,17,19,42,13,0]
+        mylist = UnorderedList()
+        mylist.add(12)
+        mylist.add(22)
+        mylist.add(9)
+        mylist.add(89)
+        mylist.add(71)
+        mylist.add(16)
 
+        self.testlist = mylist.toList()
 
     def test_sequentialSearch(self):
         self.assertEqual(sequentialSearch(self.testlist,3),False)
-        self.assertEqual(sequentialSearch(self.testlist,13),True)
+        self.assertEqual(sequentialSearch(self.testlist,16),True)
 
+
+class orderedSequentialSearchTestCase(unittest.TestCase):  
+
+    def setUp(self):
+        mylist = OrderedList()
+        mylist.add(12)
+        mylist.add(22)
+        mylist.add(9)
+        mylist.add(89)
+        mylist.add(71)
+        mylist.add(16)
+
+        self.testlist = mylist.toList()
+
+    def test_sequentialSearch(self):
+        self.assertEqual(sequentialSearch(self.testlist,3),False)
+        self.assertEqual(sequentialSearch(self.testlist,16),True)
 
 
 if __name__ == '__main__':
