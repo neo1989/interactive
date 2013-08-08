88a5122c1068a75266964b083f7c0c91c428f5d0
neo1989 <neosu89@gmail.com>
2013-08-08 07:17:26 -0700

    add sort

---
+++ lib/sort.py
@@ -0,0 +1,13 @@
+#coding:utf-8
+
+def bubbleSort(alist):
+    """冒泡排序"""
+    for passnum in range(len(alist)-1,0,-1):
+        for i in range(passnum):
+            if alist[i]>alist[i+1]:
+                temp = alist[i]
+                alist[i] = alist[i+1]
+                alist[i+1] = temp
+
+    return alist
+
---
+++ tests/test_sort.py
@@ -0,0 +1,29 @@
+import sys
+sys.path.append("lib")
+
+import unittest
+from sort import * 
+from lists import * 
+
+
+class bubbleSortTestCase(unittest.TestCase):  
+
+    def setUp(self):
+        mylist = UnorderedList()
+        mylist.add(12)
+        mylist.add(22)
+        mylist.add(9)
+        mylist.add(89)
+        mylist.add(71)
+        mylist.add(16)
+
+        self.testlist = mylist.toList()
+    
+    def test_sort(self):
+        self.assertEqual(bubbleSort(self.testlist),[9,12,16,22,71,89])
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
