ed7566b38c621eb2c62c9019e9c8977bb7bc7c0f
neo1989 <neosu89@gmail.com>
2013-08-14 17:48:56 +0800

    add sort functions

--- lib/sort.py
+++ lib/sort.py
@@ -11,3 +11,39 @@ def bubbleSort(alist):
 
     return alist
 
+def selectSort(alist):
+    """选择排序"""
+    for fillslot in range(len(alist)-1,0,-1):
+        positionOfMax = 0
+        for location in range(1,fillslot+1):
+            if alist[location] > alist[positionOfMax]:
+                positionOfMax = location
+
+        tmp = alist[fillslot]
+        alist[fillslot] = alist[positionOfMax]
+        alist[positionOfMax] = tmp
+
+    return alist
+
+def insertionSort(alist):
+    """插入排序"""
+    for index in range(1,len(alist)):
+        currentValue =  alist[index]
+        position = index
+
+        while position > 0 and alist[position-1] > currentValue:
+            alist[position] = alist[position-1]
+            position = position - 1
+        
+        alist[position] = currentValue
+
+    return alist
+    
+def shellSort(alist):
+    """希尔排序"""
+    pass
+
+
+
+
+        
--- tests/test_sort.py
+++ tests/test_sort.py
@@ -22,6 +22,41 @@ class bubbleSortTestCase(unittest.TestCase):
     def test_sort(self):
         self.assertEqual(bubbleSort(self.testlist),[9,12,16,22,71,89])
 
+class selectSortTestCase(unittest.TestCase):
+
+    def setUp(self):
+        mylist = UnorderedList()
+        mylist.add(12)
+        mylist.add(22)
+        mylist.add(9)
+        mylist.add(89)
+        mylist.add(71)
+        mylist.add(16)
+
+        self.testlist = mylist.toList()
+
+    def test_sort(self):
+        self.assertEqual(selectSort(self.testlist),[9,12,16,22,71,89])
+
+class insertionSortTestCase(unittest.TestCase):
+
+    def setUp(self):
+        mylist = UnorderedList()
+        mylist.add(12)
+        mylist.add(22)
+        mylist.add(9)
+        mylist.add(89)
+        mylist.add(71)
+        mylist.add(16)
+
+        self.testlist = mylist.toList()
+
+    def test_sort(self):
+        self.assertEqual(insertionSort(self.testlist),[9,12,16,22,71,89])
+
+
+
+
 
 if __name__ == "__main__":
     unittest.main()
